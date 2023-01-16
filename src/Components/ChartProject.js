f6771313be52246233b9e409bfe34e6121005aa8
kokivasileski <thekoki001@gmail.com>
2023-01-16 09:03:05 -0500

    adjusted project image and text layout on smaller screen sizes

--- src/Components/ChartProject.js
+++ src/Components/ChartProject.js
@@ -30,22 +30,14 @@ const ChartProject = function () {
         React <span>||</span> Redux <span>||</span> SASS <span>||</span> MUI
       </h4>
       <p>
-        Using the{" "}
-        <a
-          className="rechartsLink"
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://recharts.org/en-US/"
-        >
-          Recharts
-        </a>{" "}
-        library, I wanted to create a fully dynamic graphing site, which is both
-        customizable and easy to use. <br /> <br /> Users are able to pick a
-        chart type from a drop-down menu, and start visualizing their data live.
-        Once the chart is filled out, users can also download an image of their
-        chart. <br /> <br /> For now, Redux is only lightly used. Once every
-        chart type is available, I would like for users to be able to switch
-        between different types of charts without having to re-enter their data.
+        Using the Recharts library, I wanted to create a fully dynamic graphing
+        site, which is both customizable and easy to use. <br /> <br /> Users
+        are able to pick a chart type from a drop-down menu, and start
+        visualizing their data live. Once the chart is filled out, users can
+        also download an image of their chart. <br /> <br /> For now, Redux is
+        only lightly used. Once every chart type is available, I would like for
+        users to be able to switch between different types of charts without
+        having to re-enter their data.
       </p>
       <img className="wrenchGif" src="./assets/wrench.gif" />
     </div>
